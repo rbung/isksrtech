736a318fec4d5d4dac27aaff1e29576aef4d4900
rbung <rkbung@gmail.com>
2014-01-18 18:15:51 +0100

    more techniques

--- src/main/resources/index_creation.sh
+++ src/main/resources/index_creation.sh
@@ -1,16 +1,50 @@
 #!/bin/sh
 curl -XPUT 'http://localhost:9200/techniques/technique/1' -d '{
-    "name" : "technique1"
+    "name" : "mae ryusui geri"
 }'
 curl -XPUT 'http://localhost:9200/techniques/technique/2' -d '{
-    "name" : "technique2"
+    "name" : "kote nuki"
 }'
 curl -XPUT 'http://localhost:9200/techniques/technique/3' -d '{
-    "name" : "technique3"
+    "name" : "gyaku gote"
 }'
 curl -XPUT 'http://localhost:9200/techniques/technique/4' -d '{
-    "name" : "technique4"
+    "name" : "ushiro ryusu geri"
 }'
 curl -XPUT 'http://localhost:9200/techniques/technique/5' -d '{
-    "name" : "technique5"
+    "name" : "katate maki nuki"
 }'
+curl -XPUT 'http://localhost:9200/techniques/technique/6' -d '{
+    "name" : "katate okuri gote"
+}'
+curl -XPUT 'http://localhost:9200/techniques/technique/7' -d '{
+    "name" : "uwa uke geri"
+}'
+curl -XPUT 'http://localhost:9200/techniques/technique/8' -d '{
+    "name" : "katate juji nuki"
+}'
+curl -XPUT 'http://localhost:9200/techniques/technique/9' -d '{
+    "name" : "seï juji gote"
+}'
+curl -XPUT 'http://localhost:9200/techniques/technique/10' -d '{
+    "name" : "uchi uke geri"
+}'
+curl -XPUT 'http://localhost:9200/techniques/technique/11' -d '{
+    "name" : "katate johaku nuki"
+}'
+curl -XPUT 'http://localhost:9200/techniques/technique/12' -d '{
+    "name" : "katate johaku dori"
+}'
+curl -XPUT 'http://localhost:9200/techniques/technique/13' -d '{
+    "name" : "uchi uke zuki"
+}'
+curl -XPUT 'http://localhost:9200/techniques/technique/14' -d '{
+    "name" : "katate yori nuki"
+}'
+curl -XPUT 'http://localhost:9200/techniques/technique/15' -d '{
+    "name" : "okuri maki tembin"
+}'
+curl -XPUT 'http://localhost:9200/techniques/technique/16' -d '{
+    "name" : "soto uke geri"
+}'
+
